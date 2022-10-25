56d39ab80a4cf18efb72db7021f6a1b2d331354f
Demmynile <balogun80@gmail.com>
2022-10-25 07:35:26 +0100

    New changes done in the frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
-import type , {NextPage} from "next"
+import type , {GetStaticProps, NextPage} from "next"
 import Head from "next/head"
 import About from "../components/About"
 import Header from "../components/Header"
 import Hero from "../components/Hero"
 import WorkExperience from "../components/WorkExperience"
+import { Experience, PageInfo, Project, Skill, Social } from "../typings"
+import { fetchExperiences } from "../utils/fetchExperiences"
+import { fetchPageInfo } from "../utils/fetchPageInfo"
+import { fetchProjects } from "../utils/fetchProjects"
+import { fetchSkills } from "../utils/fetchSkills"
+import { fetchSocial } from "../utils/fetchSocials"
 
 
 
-const Home : NextPage = () => {
+type Props = {
+	pageInfo : PageInfo;
+	experiences: Experience[];
+	skills : Skill[] ;
+	projects : Project[];
+	socials: Social[];
+}
+
+
+const Home  = ({projects, skills, socials , experiences, pageInfo} : Props) => {
 	return (
 		<div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
 			<Head>
@@ -40,4 +55,27 @@ const Home : NextPage = () => {
 	)
 }
 
-export default Home
+export default Home;
+
+export const getStaticProps : GetStaticProps<Props> = async() => {
+
+	const pageInfo: PageInfo= await fetchPageInfo();
+	const experiences: Experience[] = await fetchExperiences();
+	const skills: Skill[] = await fetchSkills();
+	const projects: Project[] = await fetchProjects();
+	const socials: Social[] = await fetchSocial();
+
+return {
+  props : {
+	pageInfo,
+	experiences,
+	skills,
+	projects,
+	socials
+
+  },
+
+  revalidate: 10,
+}
+
+}
